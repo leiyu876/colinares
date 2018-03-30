8b493a8ba568c7c915c8e1d80367fa6e6b6fb9f8
Leo Nuneza <leiyu876@yahoo.com>
2018-03-30 12:23:01 +0300

    check hook

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Colinares Family
                 </div>
 
                 <div class="links">
