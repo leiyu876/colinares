37ff09c2fbb3bab03f86d5bc5169530436180b2a
Leo Nuneza <leiyu876@yahoo.com>
2018-04-19 13:29:56 +0300

    need auth in sending email

--- app/Http/Controllers/LeoemailwaysController.php
+++ app/Http/Controllers/LeoemailwaysController.php
@@ -13,6 +13,11 @@ use App\Jobs\SendEmailViaMailable;
 
 class LeoemailwaysController extends Controller
 {
+    public function __construct() {
+
+        $this->middleware('auth');
+    }
+    
 	public function leo_do_email_before()
 	{
 		$data = array(
