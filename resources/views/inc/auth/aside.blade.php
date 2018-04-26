02828514b7d051039f3148c7b6ef4b631d7b5801
Leo D. Nuneza <leiyu876@yahoo.com>
2018-04-26 11:27:45 +0300

    aside menu highlight if selected

--- resources/views/inc/auth/aside.blade.php
+++ resources/views/inc/auth/aside.blade.php
@@ -25,41 +25,41 @@
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
       <li class="header">MAIN NAVIGATION</li>
-      <li><a href="{{ url('home') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-      <li class="treeview">
+      <li class="{{ Request::is('home') ? 'menu-open' : '' }}"><a href="{{ url('home') }}"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+      <li class="treeview {{ Request::is('users*') ? 'menu-open' : '' }}">
         <a href="#">
           <i class="fa fa-users"></i> <span>Users</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
-        <ul class="treeview-menu">
-          <li><a href="{{ url('users') }}"><i class="fa fa-circle-o"></i> Users List</a></li>
-          <li><a href="{{ url('users/create') }}"><i class="fa fa-circle-o"></i> Create User</a></li>
+        <ul class="treeview-menu" style="{{ Request::is('users*') ? 'display:block' : '' }}">
+          <li  class="{{ Request::is('users') ? 'active' : '' }}"><a href="{{ url('users') }}"><i class="fa fa-circle-o"></i> Users List</a></li>
+          <li  class="{{ Request::is('users/create') ? 'active' : '' }}"><a href="{{ url('users/create') }}"><i class="fa fa-circle-o"></i> Create User</a></li>
         </ul>
       </li>
-      <li class="treeview">
+      <li class="treeview {{ Request::is('applicants') || Request::is('agencies') ? 'menu-open' : '' }}">
         <a href="#">
           <i class="fa fa-users"></i> <span>OFW</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
-        <ul class="treeview-menu">
-          <li><a href="{{ route('applicants.index') }}"><i class="fa fa-circle-o"></i> Applicants</a></li>
-          <li><a href="{{ route('agencies.index') }}"><i class="fa fa-circle-o"></i> Agencies</a></li>
+        <ul class="treeview-menu" style="{{ Request::is('applicants') || Request::is('agencies') ? 'display:block' : '' }}">
+          <li  class="{{ Request::is('applicants') ? 'active' : '' }}"><a href="{{ route('applicants.index') }}"><i class="fa fa-circle-o"></i> Applicants</a></li>
+          <li  class="{{ Request::is('agencies') ? 'active' : '' }}"><a href="{{ route('agencies.index') }}"><i class="fa fa-circle-o"></i> Agencies</a></li>
         </ul>
       </li>
-      <li class="treeview">
+      <li class="treeview {{ Request::is('movies*') ? 'menu-open' : '' }}">
         <a href="#">
           <i class="fa fa-video-camera"></i> <span>Movies</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
-        <ul class="treeview-menu">
-          <li><a href="{{ route('movies.index') }}"><i class="fa fa-circle-o"></i> Movie List</a></li>
-          <li><a href="{{ url('movies/create') }}"><i class="fa fa-circle-o"></i> Create Movie</a></li>
+        <ul class="treeview-menu" style="{{ Request::is('movies*') ? 'display:block' : '' }}">
+          <li class="{{ Request::is('movies') ? 'active' : '' }}"><a href="{{ route('movies.index') }}"><i class="fa fa-circle-o"></i> Movie List</a></li>
+          <li class="{{ Request::is('movies/create') ? 'active' : '' }}"><a href="{{ url('movies/create') }}"><i class="fa fa-circle-o"></i> Create Movie</a></li>
         </ul>
       </li>
     </ul>
