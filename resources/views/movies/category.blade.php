bce57d754a0194781c65b57a444705047c7f41e5
Leo D. Nuneza <leiyu876@yahoo.com>
2018-05-13 09:47:33 +0300

    add request form to the public movie listing

--- resources/views/movies/category.blade.php
+++ resources/views/movies/category.blade.php
@@ -7,16 +7,23 @@
 	</div>
 
 	<div class="container">
-		
 		<div class="row" >
-			@foreach($movies as $movie)
-				<a href="{{ route('movies.single', $movie->slug) }}">
-					<div class="col-md-4" style="padding: 15px">
-						<img src="{{ asset('storage/'.displayImage($movie->image)) }}" width="360" height="309.23">
-						<h3>{{ $movie->title }}</h3>
-					</div>	
-				</a>
-			@endforeach
+			<div class="col-sm-9">
+				<div class="row" >
+					@foreach($movies as $movie)				
+						<a href="{{ route('movies.single', $movie->slug) }}">						
+							<div class="col-sm-4" style="padding: 15px">
+								<img src="{{ asset('storage/'.displayImage($movie->image)) }}" width="260" height="200">
+								<h4>{{ $movie->title }}</h4>
+							</div>							
+						</a>
+					@endforeach
+				</div>	
+			</div>
+			<div class="col-sm-3" style="padding: 15px">
+				<h5>Request a movie? Comment here.</h5>
+					<div class="fb-comments" data-href="{{ url('movies/category') }}" data-numposts="10" data-width="100%"></div>
+			</div>	
 		</div>
 		<div class="row" style="text-align: center;">{{ $movies->links() }}</div>
 	</div>
