d75d9788410a23f80781d9836b267ab6fef42756
Leo D. Nuneza <leiyu876@yahoo.com>
2018-05-17 12:21:13 +0300

    font colinares if resize

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,6 +19,13 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js') }}"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js') }}"></script>
     <![endif]-->
+    <style type="text/css">
+    	@media (max-width: 465px) {
+	    	a.navbar-brand span{
+				font-size: 16px;
+			}
+		}
+	</style>
     @yield('css')
   </head>
   <!-- Global site tag (gtag.js) - Google Analytics -->
