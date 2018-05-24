96f278e044d70f922a79f55e03ead51be3e4c6fc
Leo D. Nuneza <leiyu876@yahoo.com>
2018-05-24 12:23:28 +0300

    change some text in contact us

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -12,7 +12,7 @@
 			<div class="row">
 				<div class="col-lg-6">
 					<div class="recent">
-						<h3>Send us a message</h3>
+						<h3>Send us a message(under construction :())</h3>
 					</div>		
 					<form role="form">
 						<div class="form-group">						
@@ -23,7 +23,7 @@
 						</div>
 						
 						<textarea class="form-control" rows="8"></textarea>
-						<button type="submit" class="btn btn-default">Submit</button>
+						<!--<button type="submit" class="btn btn-default">Submit</button>-->
 					</form>
 				</div>
 				
@@ -32,18 +32,14 @@
 						<h3>Get in touch with us</h3>
 					</div>
 					<div class="">
-						<p>Nam liber tempor cum soluta nobis eleifend option 
-						congue nihil imperdiet doming id quod mazim placerat
-						facer possim assum. Typi non habent claritatem insitam;
-						est usus legentis in iis qui facit eorum.</p>
-						<p>Nam liber tempor cum soluta nobis eleifend option 
-						congue nihil imperdiet doming id quod mazim placerat
-						facer possim assum. Typi non habent claritatem insitam;
-						est usus legentis in iis qui facit eorum.</p>
+						<p>Pedro: <i> Manong bat ang liit liit mo?</i></p>
+						<p>Manong <i>Unano: Kasi baby pa lang ako nang ako ay mawalan ng magulang.</i></p>
+						<p>Pedro: <i>E ano naman ang koneksyon nunsa pagiging maliit mo?</i></p>
+						<p>Manong <i>Unano: Tanga ka ba? ehh di walang nag palaki sakin.</i></p>
 						
-						<h4>Address:</h4>Little Lonsdale St, New York<br>
-						<h4>Telephone:</h4>345 578 59 45 416</br>
-						<h4>Fax:</h4>123 456 789
+						<h4>Address:</h4>Sacred Heart Village, Corazon Catmon Cebu, Philippines<br>
+						<h4>Telephone:</h4>I dont have :)</br>
+						<h4>Facebook:</h4>https://www.facebook.com/leiyu876
 					</div>										
 				</div>			
 			</div>
