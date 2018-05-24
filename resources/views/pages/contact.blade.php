1e9a8f700005842e84f2b041faffd7a3a0f328df
Leo D. Nuneza <leiyu876@yahoo.com>
2018-05-24 12:08:01 +0300

    change contact map

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -5,12 +5,9 @@
 	<div class="breadcrumb">
 		<h2>Contact</h2>
 	</div>
-	
 	<div class="map">
-		<iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Kuningan,+Jakarta+Capital+Region,+Indonesia&amp;aq=3&amp;oq=kuningan+&amp;sll=37.0625,-95.677068&amp;sspn=37.410045,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Kuningan&amp;t=m&amp;z=14&amp;ll=-6.238824,106.830177&amp;output=embed">
-		</iframe>
+		<iframe src="https://www.google.com/maps/d/embed?mid=1Qrryb_yVZmNJWWBARkHcH81R7CFv_HjZ"></iframe>
 	</div>
-
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-6">
