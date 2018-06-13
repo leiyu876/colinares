f2dea79b0d159ae8181fdbbb8326ab8e481b65a7
Leo D. Nuneza <leiyu876@yahoo.com>
2018-06-13 10:50:00 +0300

    embded change content to video only

--- resources/views/pages/testpage.blade.php
+++ resources/views/pages/testpage.blade.php
@@ -6,6 +6,6 @@
 		<h2>Events</h2>
 	</div>
 	<div class="container">
-		<<iframe width='1080' height='760' src="https://pinoytva.ch/watch/sarah-ang-munting-prinsesa-full-57799.html" frameborder="0" allowfullscreen></iframe>
+		<<iframe width='1080' height='760' src="https://streamango.com/embed/acrrqffaefclakmf/01223_MP4_mp4" frameborder="0" allowfullscreen></iframe>
 	</div>
 @endsection
